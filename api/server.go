089468c0738139485449815fd53675dd89f7b071
Alexander <kozhukhovmib@gmail.com>
2023-03-12 22:10:21 +0300

    added delete route

--- api/server.go
+++ api/server.go
@@ -18,7 +18,8 @@ func NewServer(store *db.Store) *Server {
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
-
+	router.DELETE("/accounts/:id", server.deleteAccount)
+	// TODO: add router.UPDATE
 	server.router = router
 	return server
 }
